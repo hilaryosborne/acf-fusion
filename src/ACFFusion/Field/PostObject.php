420a504c833d21b678911979d119aa401b0a6e19
Hilary Osborne <h.t.osborne@gmail.com>
2017-10-24 22:03:29 +1000

    Added missing post object setter methods

--- src/ACFFusion/Field/PostObject.php
+++ src/ACFFusion/Field/PostObject.php
@@ -29,4 +29,46 @@ class PostObject extends Field {
 
     public static $type = 'field';
 
+    public function setPostType($value) {
+        // Set the setting option value
+        $this->settings['post_type'] = (array)$value;
+        // Return for chaining
+        return $this;
+    }
+
+    public function setTaxonomy($value) {
+        // Set the setting option value
+        $this->settings['taxonomy'] = (array)$value;
+        // Return for chaining
+        return $this;
+    }
+
+    public function setAllowNull($value) {
+        // Set the setting option value
+        $this->settings['multiple'] = (bool)$value;
+        // Return for chaining
+        return $this;
+    }
+
+    public function setMultiple($value) {
+        // Set the setting option value
+        $this->settings['taxonomy'] = (bool)$value;
+        // Return for chaining
+        return $this;
+    }
+
+    public function setReturnFormat($value) {
+        // Set the setting option value
+        $this->settings['return_format'] = (string)$value;
+        // Return for chaining
+        return $this;
+    }
+
+    public function setUI($value) {
+        // Set the setting option value
+        $this->settings['ui'] = (bool)$value;
+        // Return for chaining
+        return $this;
+    }
+
 }
